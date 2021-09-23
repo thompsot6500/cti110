35330ddd784c9c9a9e4c2c5b0990a05232b605bd
thompsot6500 <90720024+thompsot6500@users.noreply.github.com>
2021-09-23 14:25:21 -0400

    p1hw1_output_thompsontyler

---
+++ P1Hw1_output_thompsontyler.py
@@ -0,0 +1,10 @@
+user_num = int(input('Enter integer:\n'))
+
+print("You entered:", user_num)
+print(user_num, "squared is", user_num*user_num)
+print("And", user_num, "cubed is", user_num*user_num*user_num, "!!")
+
+user_num2 = int(input("Enter another integer:\n"))
+
+print(user_num, "+", user_num2, "is", int(user_num)+int(user_num2))
+print(user_num, "*", user_num2, "is", int(user_num)*int(user_num2))
